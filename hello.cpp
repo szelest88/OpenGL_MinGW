e534ef8bb6ef8a7a1c1ea92f039792c86b31ba86
szelest <szelest88@gmail.com>
2014-08-23 19:04:30 +0200

    Initialize OpenGL :)

--- hello.cpp
+++ hello.cpp
@@ -1,37 +1,33 @@
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_opengl.h"
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
 
-    SDL_Window *window;                    // Declare a pointer
+SDL_Init(SDL_INIT_VIDEO);
+SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
 
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+SDL_GLContext context = SDL_GL_CreateContext(window);
 
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
-
-    // Check that the window was successfully made
-    if (window == NULL) {
-        // In the event that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
+SDL_Event windowEvent;
+while(true){
+    if(SDL_PollEvent(&windowEvent))
+    {
+        if (
+            windowEvent.type == SDL_QUIT
+            ||
+            windowEvent.type == SDL_KEYUP && windowEvent.key.keysym.sym == SDLK_ESCAPE
+            ) break;
     }
 
-    // The window is open: enter program loop (see SDL_PollEvent)
+    SDL_GL_SwapWindow(window);
+}
 
-    SDL_Delay(10000);  // Pause execution for 3000 milliseconds, for example
+SDL_GL_DeleteContext(context);
 
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-
-    // Clean up
-    SDL_Quit();
+SDL_Quit();
     return 0;
 }
