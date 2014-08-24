0f99b7cd0a6217433ed6616d03b64c5e8a29d7fc
szelest <szelest88@gmail.com>
2014-08-24 15:22:50 +0200

    Add and initialize glew and update build command

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,9 @@
+#include <GL/glew.h> // has to be included before SDL
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 #include <stdio.h>
+#include <iostream>
 
 int main(int argc, char* argv[]) {
 
@@ -8,9 +11,20 @@ SDL_Init(SDL_INIT_VIDEO);
 SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+
+
 SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
 
 SDL_GLContext context = SDL_GL_CreateContext(window);
+
+glewExperimental = GL_TRUE;
+glewInit(); // has to be initialized after creating a window and the context
+
+GLuint vertexBuffer; // now test if glew has been initialized correctly
+glGenBuffers(1, &vertexBuffer);
+
+std::cout<<vertexBuffer;
 
 SDL_Event windowEvent;
 while(true){
